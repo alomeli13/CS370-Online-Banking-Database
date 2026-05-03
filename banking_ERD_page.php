6a953d39b429a386995bad1545bf73c90511fc49
SednaplanetX <sedna0735@gmail.com>
2026-05-02 20:02:56 -0500

    First commit for the ERD page showing a PNG image of the ERD.

---
+++ banking_ERD_page.php
@@ -0,0 +1,32 @@
+<?php include('_header.php');?>
+
+    <style>
+
+        h1 {
+            border-bottom: 2px solid #333;
+            padding-bottom: 5px;
+            margin-left: 460px;
+            margin-right: 460px;
+        }
+        
+        img {
+            max-width: 100%;
+            height: auto;
+            border: 1px solid #dee2e6;
+            border-radius: 5px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        }
+        
+    </style>
+
+    <body>
+
+        <h1 class="text-center mt-4">Banking Database ERD</h1>
+
+        <div class="d-flex justify-content-center align-items-center" style="min-height: 100vh;">
+            <img src="banking_ERD_image.png" alt="An ERD of the Banking Database.">
+        </div>
+
+    </body>
+
+<?php include('_footer.php');?>
